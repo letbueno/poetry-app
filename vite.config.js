a3e847edf0df696fc441f7ea5dd459935f169031
letbueno <leticia.bueno.martins@gmail.com>
2024-10-30 10:03:55 -0300

    fix: base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/poetry_app' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/poetry-app' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
