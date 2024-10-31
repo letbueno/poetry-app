8d06a3eabeed1ca12984f8abd7a8dbe32de2190b
letbueno <leticia.bueno.martins@gmail.com>
2024-10-31 00:00:27 -0300

    fix: vite.config.js resolve

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,14 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import { resolve } from 'path'
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/poetry-app/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/poetry-app' : '/',
   plugins: [vue()],
-
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: resolve(__dirname, 'index.html'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
