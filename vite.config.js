f97bd6df22d3ed31d591b4d80b1af235f0bc867b
letbueno <leticia.bueno.martins@gmail.com>
2024-10-30 22:57:06 -0300

    fix: change base on vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/poetry-app' : '/',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
